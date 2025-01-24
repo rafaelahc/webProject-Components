2fece67057ae5dc51f520b8288763c3d26ca0cb5
Rafaela Carvalho <rafaelah05@hotmail.com>
2025-01-24 16:50:01 +0000

    verão

--- script.js
+++ script.js
@@ -5,25 +5,24 @@ const container = document.querySelector(".container");
 
 function createLeaves() {
   // const clone = leave.cloneNode(true);  
-  // clone.style.paddingLeft = Math.random() * 10 + "px"; // creating left padding  
-  // clone.style.animationDuration = Math.random() * 5 + 3 + "s"; // animation duration between 3-5  
+  // clone.style.paddingLeft = Math.random() * 10 + "px";
+  // clone.style.animationDuration = Math.random() * 5 + 3 + "s"; 
   // clone.style.opacity = Math.random() * 1;  
-  // container.append(clone); // adding clone flake to container
+  // container.append(clone); 
 
-  // const snowFlake = snow.cloneNode(true);
-  // snowFlake.style.paddingLeft = Math.random() * 10 + "px"; // creating left padding
-  // snowFlake.style.animationDuration = Math.random() * 5 + 3 + "s"; // animation duration between 3-5
-  // snowFlake.style.opacity = Math.random() * 1;
-  // container.append(snowFlake); // adding clone flake to container
+  const snowFlake = snow.cloneNode(true);
+  snowFlake.style.paddingLeft = Math.random() * 10 + "px"; 
+  snowFlake.style.animationDuration = Math.random() * 5 + 3 + "s"; 
+  snowFlake.style.opacity = Math.random() * 1;
+  container.append(snowFlake); 
 
-  flower.forEach(flor => {
-    const flowers = flor.cloneNode(true);
-    flowers.style.paddingLeft = Math.random() * 10 + "px"; // creating left padding
-    flowers.style.animationDuration = Math.random() * 5 + 3 + "s"; // animation duration between 3-5
-    flowers.style.opacity = Math.random() * 1;
-    container.append(flowers); // adding clone flake to container
-  });
-
+  // flower.forEach(flor => {
+  //   const flowers = flor.cloneNode(true);
+  //   flowers.style.paddingLeft = Math.random() * 10 + "px"; 
+  //   flowers.style.animationDuration = Math.random() * 5 + 3 + "s"; 
+  //   flowers.style.opacity = Math.random() * 1;
+  //   container.append(flowers); 
+  // });
   
 }
 
